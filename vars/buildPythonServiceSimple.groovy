6d95e8a12f6784a2bafdce37fea0d46cf88cdc05
Rajan Patil <r.patil@elsevier.com>
2020-04-21 12:27:28 +0100

    added simple pipeline

---
+++ vars/buildPythonServiceSimple.groovy
@@ -0,0 +1,52 @@
+import com.shared.lib.PythonServiceBuildSteps
+
+
+def call(Map config) {
+
+    def buildSteps = new PythonServiceBuildSteps(this)
+
+    pipeline {
+
+        agent any
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Install Dependencies') {
+                steps {
+                    script {
+                        buildSteps.install()
+                    }
+                }
+            }
+
+            stage('Check Code Style and Formatting') {
+                steps {
+                    script {
+                        buildSteps.checkCodeStyle()
+                    }
+                }
+            }
+
+            stage('Run Tests') {
+                steps {
+                    script {
+                        buildSteps.runTests()
+                    }
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    script {
+                        buildSteps.build()
+                    }
+                }
+            }
+        }
+    }
+
+}
