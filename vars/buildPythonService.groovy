aeb01ee3057c5745a4129eb608db472e1d568275
Rajan Patil <r.patil@elsevier.com>
2020-04-21 11:42:31 +0100

    change mergeConfig function place

--- vars/buildPythonService.groovy
+++ vars/buildPythonService.groovy
@@ -1,5 +1,31 @@
 import com.shared.lib.PythonServiceBuildSteps
 
+def mergeConfig(Map config) {
+    Map defaultConfig = [
+            install: [
+                    steps: PythonServiceBuildSteps.defaultInstallSteps
+            ],
+            checkStyle: [
+                    steps: PythonServiceBuildSteps.defaultCheckCodeStyleSteps
+            ],
+            tests: [
+                    steps: PythonServiceBuildSteps.defaultRunTestsSteps
+            ],
+            build: [
+                    steps: PythonServiceBuildSteps.defaultBuildSteps
+            ]
+    ]
+
+    Map result = [:]
+    [defaultConfig, config].each { map ->
+        map.each { key, value ->
+            result[key] = result[key] instanceof Map ? mergeConfig(result[key], value) : value
+        }
+    }
+
+    result
+}
+
 def call(Map config) {
 
     def buildSteps = new PythonServiceBuildSteps(this)
@@ -53,31 +79,4 @@ def call(Map config) {
         }
     }
 
-}
-
-
-def mergeConfig(Map config) {
-    Map defaultConfig = [
-            install: [
-                    steps: PythonServiceBuildSteps.defaultInstallSteps
-            ],
-            checkStyle: [
-                    steps: PythonServiceBuildSteps.defaultCheckCodeStyleSteps
-            ],
-            tests: [
-                    steps: PythonServiceBuildSteps.defaultRunTestsSteps
-            ],
-            build: [
-                    steps: PythonServiceBuildSteps.defaultBuildSteps
-            ]
-    ]
-
-    Map result = [:]
-    [defaultConfig, config].each { map ->
-        map.each { key, value ->
-            result[key] = result[key] instanceof Map ? mergeConfig(result[key], value) : value
-        }
-    }
-
-    result
 }
