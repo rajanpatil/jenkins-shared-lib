122083acf4a783725ee29ca49489fb703d14431b
Rajan Patil <r.patil@elsevier.com>
2020-05-06 17:19:25 +0100

    fixed issue

--- src/com/shared/lib/ConfigManager.groovy
+++ src/com/shared/lib/ConfigManager.groovy
@@ -24,7 +24,7 @@ class ConfigManager implements Serializable {
     private Map config
 
     ConfigManager(Map config, script) {
-        String repoName = getRepoName(script.GIT_URL)
+        String repoName = getRepoName(script.env.GIT_URL)
 
         defaultConfig.put("repoName", repoName)
 
